8c3c10f14b3fa10e635c08bf95a1627c7ff79eac
natpowning <nat@powning.org>
2020-02-18 07:06:35 -0800

    Initial starting point with AWS SDK printing SG's.

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/natpowning/aws2terraform
+
+go 1.13
+
+require (
+	github.com/aws/aws-sdk-go v1.29.4 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func main() {
+	svc := ec2.New(session.New())
+	input := &ec2.DescribeSecurityGroupsInput{}
+
+	result, err := svc.DescribeSecurityGroups(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result)
+}
